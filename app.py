7706197d712acf7c38187f26df06902ffdd1734f
zenmastercat <lucas.chaleenon.poptie@gmail.com>
2025-07-13 15:38:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__, static_folder='static')
 
 model = pickle.load(open("model.pkl", "rb"))
 
+if len(features[0]) == 0:
+    return jsonify({ "result": "Please type the sentence first." })
+
+
 
 @app.route("/")
 def index():
